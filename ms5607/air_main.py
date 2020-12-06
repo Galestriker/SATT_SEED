8d1204cfe64e137f64457cd26fc17f5572f23c55
Galestriker <gale.striker745578@gmail.com>
2020-12-06 19:58:07 +0900

    air main 変更

--- ms5607/air_main.py
+++ ms5607/air_main.py
@@ -3,27 +3,19 @@ from MS5607 import MS5607
 import csv
 import RPi.GPIO as GPIO
 
-SERVO_PIN = 12
-XBee_SLEEP_PIN = 15
-ALT1 = 10                   #切り離しを可能にする高度
-ALT2 = 0.2                  #切り離し高度
-TIME1 = 1                   #上昇中のデータ取得間隔
-TIME2 = 0.1                 #落下中のデータ取得間隔
-FREQ = 100                  #PWMの周波数
-ANGLE = 45                  #サーボ回転角度
-ONCE = True
-BACKUP = 2                  #バックアップの切り離し判定高度
-BACKUP_DETACH_TIME = 60     #バックアップの切り離しまでの時間
-filename = 'output_air_main.csv'
-
-def convert_dc(deg):
-    deg_min = 0.0
-    deg_max = 180.0
-    dc_min = 5.0
-    dc_max = 22.0
-    return ((deg - deg_min) * (dc_max - dc_min)/(deg_max - deg_min) + dc_min)
-
 def air_main():
+    SERVO_PIN = 12
+    XBee_SLEEP_PIN = 15
+    ALT1 = 10                   #切り離しを可能にする高度
+    ALT2 = 0.2                  #切り離し高度
+    TIME1 = 1                   #上昇中のデータ取得間隔
+    TIME2 = 0.1                 #落下中のデータ取得間隔
+    FREQ = 50                   #PWMの周波数，S0270では100,SGでは50
+    ANGLE = 90                  #サーボ回転角度，S0270では45，SG90では90
+    ONCE = True
+    BACKUP_ALT = 2              #バックアップの切り離し判定高度
+    BACKUP_DETACH_TIME = 60     #バックアップの切り離しまでの時間
+    filename = 'output_air_main.csv'
 
     #セットアップ
     print('air_main setup start')
@@ -31,8 +23,7 @@ def air_main():
     GPIO.setup(XBee_SLEEP_PIN, GPIO.OUT)
     GPIO.output(XBee_SLEEP_PIN, GPIO.LOW)
     print('Xbee sleep off')
-    #ここでXbeeのスイッチを切り替え
-
+    
     GPIO.setup(SERVO_PIN, GPIO.OUT)
     servo = GPIO.PWM(SERVO_PIN, FREQ)
     servo.start(0)
@@ -43,7 +34,6 @@ def air_main():
         writer.writerow(['Temperature (degree C)','Pressure (hPa)','Altitude (m)'])
 
     #最初のデータをはじく
-    time.sleep(1)
     for i in range(3):
         temperature = ms5607.getDigitalTemperature()
         pressure = ms5607.getDigitalPressure()
@@ -52,10 +42,10 @@ def air_main():
         time.sleep(1)
 
     print("Temperature: {} degrees C".format(tmp))
-    print("Pressure(hPa): {}".format(converted/100))
+    print("Default Pressure(hPa): {}".format(converted/100))
     print('air_main setup finish')
     print('Turn the switch.')
-
+    #ここでXbeeのスイッチを切り替え
 
 
     input("Are you ready?")
@@ -90,7 +80,7 @@ def air_main():
         tmp = sensor.getTemperature()
 
         if altitude < ALT2:
-            dc = convert_dc(float(ANGLE))
+            dc = convert_dc_SG90(float(ANGLE))
             servo.ChangeDutyCycle(dc)
             GPIO.output(XBee_SLEEP_PIN, GPIO.LOW)
 
@@ -106,12 +96,12 @@ def air_main():
             writer = csv.writer(f)
             writer.writerow([tmp, converted / 100, altitude])
 
-        if altitude < BACKUP and ONCE == True:
+        if altitude < BACKUP_ALT and ONCE == True:
             time_backup = time.time()
             ONCE = False
             
-        if　time.time() - time_backup > BACKUP_DETACH_TIME:
-            dc = convert_dc(float(ANGLE))
+        if　time.time() - time_backup > BACKUP_DETACH_TIME and ONCE == False:
+            dc = convert_dc_SG90(float(ANGLE))
             servo.ChangeDutyCycle(dc)
             GPIO.output(XBee_SLEEP_PIN, GPIO.LOW)
 
@@ -130,5 +120,23 @@ def air_main():
     #GPIO.cleanup()
     print('air_main finish')
 
+
+def convert_dc_S0270(deg):
+    deg_min = 0.0
+    deg_max = 180.0
+    dc_min = 5.0
+    dc_max = 22.0
+    return ((deg - deg_min) * (dc_max - dc_min) / (deg_max - deg_min) + dc_min)
+
+
+def convert_dc_SG90(deg):
+    deg_min = 0.0
+    deg_max = 180.0
+    dc_min = 2.5
+    dc_max = 12.0
+    return ((deg - deg_min) * (dc_max - dc_min) / (deg_max - deg_min) + dc_min)
+
+
 if __name__ == "__main__":
     air_main()
+    GPIO.cleanup()
